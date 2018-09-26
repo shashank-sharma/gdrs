c96a55b2fd7b3fbfead5edafb1b891db40b15bc9
Shashank Sharma <shashank.sharma98@gmail.com>
2018-09-26 21:54:22 +0530

    Heroku test 2

--- static/ang/inline.bundle.js
+++ static/ang/inline.bundle.js
@@ -141,7 +141,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "http://127.0.0.1:8000/static/ang/";
+/******/ 	__webpack_require__.p = "http://niti.herokuapp.com/static/ang/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
